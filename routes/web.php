a702b80f8fe72c043cbfb36d4505a60c60adb739
yunosuke <yuu0209ide@gmail.com>
2021-10-25 14:59:19 +0900

    update

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [App\Http\Controllers\BlogController::class,'showList'])->name('blogs');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
